aea96578bae01c6d2369198819d082925750ce93
tapiajulieta <julitapia77@gmail.com>
2024-01-26 10:54:49 -0300

    Mi Primera Aplicacion

---
+++ App2/AplicacionWindos2/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace AplicacionWindos2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lbl1 = new System.Windows.Forms.Label();
+            this.Lbl2 = new System.Windows.Forms.Label();
+            this.Lbl3 = new System.Windows.Forms.Label();
+            this.Lbl4 = new System.Windows.Forms.Label();
+            this.Lbl5 = new System.Windows.Forms.Label();
+            this.Txt1 = new System.Windows.Forms.TextBox();
+            this.Txt2 = new System.Windows.Forms.TextBox();
+            this.Txt3 = new System.Windows.Forms.TextBox();
+            this.Txt4 = new System.Windows.Forms.TextBox();
+            this.Btn1 = new System.Windows.Forms.Button();
+            this.Btn2 = new System.Windows.Forms.Button();
+            this.Ls1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // Lbl1
+            // 
+            this.Lbl1.AutoSize = true;
+            this.Lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl1.Location = new System.Drawing.Point(43, 26);
+            this.Lbl1.Name = "Lbl1";
+            this.Lbl1.Size = new System.Drawing.Size(67, 13);
+            this.Lbl1.TabIndex = 0;
+            this.Lbl1.Text = "APELLIDO";
+            // 
+            // Lbl2
+            // 
+            this.Lbl2.AutoSize = true;
+            this.Lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl2.Location = new System.Drawing.Point(43, 51);
+            this.Lbl2.Name = "Lbl2";
+            this.Lbl2.Size = new System.Drawing.Size(60, 13);
+            this.Lbl2.TabIndex = 1;
+            this.Lbl2.Text = "NOMBRE";
+            // 
+            // Lbl3
+            // 
+            this.Lbl3.AutoSize = true;
+            this.Lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl3.Location = new System.Drawing.Point(43, 77);
+            this.Lbl3.Name = "Lbl3";
+            this.Lbl3.Size = new System.Drawing.Size(41, 13);
+            this.Lbl3.TabIndex = 2;
+            this.Lbl3.Text = "EDAD";
+            // 
+            // Lbl4
+            // 
+            this.Lbl4.AutoSize = true;
+            this.Lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl4.Location = new System.Drawing.Point(43, 102);
+            this.Lbl4.Name = "Lbl4";
+            this.Lbl4.Size = new System.Drawing.Size(75, 13);
+            this.Lbl4.TabIndex = 3;
+            this.Lbl4.Text = "DIRECCION";
+            // 
+            // Lbl5
+            // 
+            this.Lbl5.AutoSize = true;
+            this.Lbl5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl5.Location = new System.Drawing.Point(52, 162);
+            this.Lbl5.Name = "Lbl5";
+            this.Lbl5.Size = new System.Drawing.Size(82, 13);
+            this.Lbl5.TabIndex = 4;
+            this.Lbl5.Text = "RESULTADO";
+            // 
+            // Txt1
+            // 
+            this.Txt1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Txt1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.Txt1.Location = new System.Drawing.Point(124, 19);
+            this.Txt1.Name = "Txt1";
+            this.Txt1.Size = new System.Drawing.Size(230, 20);
+            this.Txt1.TabIndex = 5;
+            // 
+            // Txt2
+            // 
+            this.Txt2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Txt2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.Txt2.Location = new System.Drawing.Point(124, 44);
+            this.Txt2.Name = "Txt2";
+            this.Txt2.Size = new System.Drawing.Size(230, 20);
+            this.Txt2.TabIndex = 6;
+            // 
+            // Txt3
+            // 
+            this.Txt3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Txt3.Location = new System.Drawing.Point(124, 70);
+            this.Txt3.Name = "Txt3";
+            this.Txt3.Size = new System.Drawing.Size(45, 20);
+            this.Txt3.TabIndex = 7;
+            this.Txt3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Txt3_KeyPress);
+            // 
+            // Txt4
+            // 
+            this.Txt4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Txt4.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.Txt4.Location = new System.Drawing.Point(124, 96);
+            this.Txt4.Name = "Txt4";
+            this.Txt4.Size = new System.Drawing.Size(230, 20);
+            this.Txt4.TabIndex = 8;
+            // 
+            // Btn1
+            // 
+            this.Btn1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Btn1.BackColor = System.Drawing.Color.Silver;
+            this.Btn1.FlatAppearance.BorderSize = 2;
+            this.Btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn1.Location = new System.Drawing.Point(111, 320);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.Size = new System.Drawing.Size(75, 23);
+            this.Btn1.TabIndex = 10;
+            this.Btn1.Text = "Aceptar";
+            this.Btn1.UseVisualStyleBackColor = false;
+            this.Btn1.Click += new System.EventHandler(this.Btn1_Click);
+            // 
+            // Btn2
+            // 
+            this.Btn2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Btn2.BackColor = System.Drawing.Color.Silver;
+            this.Btn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn2.Location = new System.Drawing.Point(242, 320);
+            this.Btn2.Name = "Btn2";
+            this.Btn2.Size = new System.Drawing.Size(75, 23);
+            this.Btn2.TabIndex = 11;
+            this.Btn2.Text = "Cancelar";
+            this.Btn2.UseVisualStyleBackColor = false;
+            this.Btn2.Click += new System.EventHandler(this.Btn2_Click);
+            // 
+            // Ls1
+            // 
+            this.Ls1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Ls1.HideSelection = false;
+            this.Ls1.Location = new System.Drawing.Point(55, 178);
+            this.Ls1.Margin = new System.Windows.Forms.Padding(15);
+            this.Ls1.Name = "Ls1";
+            this.Ls1.Size = new System.Drawing.Size(321, 122);
+            this.Ls1.TabIndex = 20;
+            this.Ls1.UseCompatibleStateImageBehavior = false;
+            this.Ls1.View = System.Windows.Forms.View.Tile;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(399, 362);
+            this.Controls.Add(this.Ls1);
+            this.Controls.Add(this.Btn2);
+            this.Controls.Add(this.Btn1);
+            this.Controls.Add(this.Txt4);
+            this.Controls.Add(this.Txt3);
+            this.Controls.Add(this.Txt2);
+            this.Controls.Add(this.Txt1);
+            this.Controls.Add(this.Lbl5);
+            this.Controls.Add(this.Lbl4);
+            this.Controls.Add(this.Lbl3);
+            this.Controls.Add(this.Lbl2);
+            this.Controls.Add(this.Lbl1);
+            this.MaximumSize = new System.Drawing.Size(415, 401);
+            this.MinimumSize = new System.Drawing.Size(415, 401);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Datos Personales";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl1;
+        private System.Windows.Forms.Label Lbl2;
+        private System.Windows.Forms.Label Lbl3;
+        private System.Windows.Forms.Label Lbl4;
+        private System.Windows.Forms.Label Lbl5;
+        private System.Windows.Forms.TextBox Txt1;
+        private System.Windows.Forms.TextBox Txt2;
+        private System.Windows.Forms.TextBox Txt3;
+        private System.Windows.Forms.TextBox Txt4;
+        private System.Windows.Forms.Button Btn1;
+        private System.Windows.Forms.Button Btn2;
+        private System.Windows.Forms.ListView Ls1;
+    }
+}
+
---
+++ App2/AplicacionWindos2/Form1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplicacionWindos2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            string elem1 = Txt1.Text;
+            string elem2 = Txt2.Text;
+            Ls1.Items.Add( "Apellido y Nombre: " + elem1 +" "+ elem2);
+
+            string elem3 = Txt3.Text;
+            Ls1.Items.Add("Edad: " + elem3);
+
+             string elem4 = Txt4.Text;
+            Ls1.Items.Add("Dirección: " + elem4);
+
+            if (Txt1.Text == "")
+                Txt1.BackColor = Color.Red;
+            else
+                Txt1.BackColor = System.Drawing.SystemColors.Control;
+
+            if (Txt2.Text == "")
+                Txt2.BackColor = Color.Red;
+            else
+                Txt2.BackColor = System.Drawing.SystemColors.Control;
+
+            if (Txt3.Text == "")
+                Txt3.BackColor = Color.Red;
+            else
+                Txt3.BackColor = System.Drawing.SystemColors.Control;
+
+            if (Txt4.Text == "")
+                Txt4.BackColor = Color.Red;
+            else
+                Txt4.BackColor = System.Drawing.SystemColors.Control;
+            
+        }
+
+        private void Txt3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 1 || e.KeyChar > 99) && e.KeyChar != 0)
+                e.Handled = true; 
+        }
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Saliendo de la app...");
+            Application.Exit();
+        }
+    }
+}
